412745ca5f6478f24a7c5559003629141cdfa732
William Werner <wwerner@ucsd.edu>
2015-02-12 19:30:21 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -14,19 +14,35 @@ function initializePage() {
 	$('#colorBtn').click(randomizeColors);
 }
 
+function callBackFn (result) {
+  var projectHTML = '<a href="#" class ="thumbnail">' + 
+    '<img src="' + result['image'] + '" class="detailsImage">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] + '</small></p></a>'+
+    '<p>' + result['summary'] +'</p>';
+
+  $("#project" + result['id']).html(projectHTML);
+
+}
+
 /*
  * Make an AJAX call to retrieve project details and add it in
  */
 function addProjectDetails(e) {
-	// Prevent following the link
-	e.preventDefault();
+  // Prevent following the link
+  e.preventDefault();
 
-	// Get the div ID, e.g., "project3"
-	var projectID = $(this).closest('.project').attr('id');
-	// get rid of 'project' from the front of the id 'project3'
-	var idNumber = projectID.substr('project'.length);
+  // Get the div ID, e.g., "project3"
+  var projectID = $(this).closest('.project').attr('id');
+  // get rid of 'project' from the front of the id 'project3'
+  var idNumber = projectID.substr('project'.length);
 
-	console.log("User clicked on project " + idNumber);
+  console.log("User clicked on project " + idNumber);
+  var projURL = "/project/" +idNumber;
+  console.log("Project URL is "+ projURL);
+  $.get(projURL, callBackFn); 
+
+
 }
 
 /*
@@ -34,5 +50,15 @@ function addProjectDetails(e) {
  * and apply it
  */
 function randomizeColors(e) {
-	console.log("User clicked on color button");
+  $.get("/palette", colorCallBack);
+}
+
+function colorCallBack (result) {
+	var tmp = result['colors'];
+	var colors = tmp['hex'];
+  $('body').css('background-color', colors[0]);
+  $('.thumbnail').css('background-color', colors[1]);
+  $('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+  $('p').css('color', colors[3]);
+  $('.project img').css('opacity', .75);
 }
